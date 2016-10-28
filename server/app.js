c8b8195d428c871a4fdb58b4a0d1797a116f835b
HenryHall <mnboogie@yahoo.com>
2016-10-28 14:05:26 -0500

    bug squashing

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.get( '/', function( req, res ){
 });
 
 
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 7000);
 app.listen(app.get('port'), function() {
   console.log('Server up:', app.get('port'));
 });
